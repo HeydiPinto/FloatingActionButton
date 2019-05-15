427a9d7eaa0eab443088fb01865c94112e335cda
Jaqueline <heydi.pinto@cbtis72.edu.mx>
2019-05-15 16:29:12 -0500

    Floating action button con menú

---
+++ app/src/main/java/com/example/floatingactionbutton/Floatingconmenu.java
@@ -0,0 +1,15 @@
+package com.example.floatingactionbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.floating.R;
+
+public class Floatingconmenu extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_floatingconmenu);
+    }
+}
