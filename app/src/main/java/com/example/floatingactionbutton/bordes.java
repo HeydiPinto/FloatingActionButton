2145ed5d978d9f18c958f77896f677750d3792e4
Jaqueline <heydi.pinto@cbtis72.edu.mx>
2019-05-14 21:47:13 -0500

    .

--- app/src/main/java/com/example/floatingactionbutton/bordes.java
+++ app/src/main/java/com/example/floatingactionbutton/bordes.java
@@ -6,29 +6,26 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.example.floating.R;
 
-public class MainActivity extends AppCompatActivity {
+public class bordes extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setContentView(R.layout.activity_bordes);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Se presionó el FAB", Snackbar.LENGTH_LONG)
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
             }
         });
     }
-
 
 }
--- app/src/main/java/com/example/floatingactionbutton/bordesfloating.java
+++
@@ -1,4 +0,0 @@
-package com.example.floatingactionbutton;
-
-public class bordesfloating {
-}
